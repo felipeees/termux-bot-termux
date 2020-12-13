22d6565200ec2b0a33b5f1269d354df1e6a07590
felipeees <75921928+felipeees@users.noreply.github.com>
2020-12-13 01:52:59 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,6 @@
+module.exports = {
+	menu: "*BY FELIPE \uD83D\uDE0E*\r\n\r\n\r\nBot Whatsapp \r\nEntre no grupo \/ link \r\nhttps:\/\/chat.whatsapp.com\/CNI4NHXMxgBHUtri3VBOvz\r\n\r\n\t\t\t  \uFE0F*Menu*\r\n\t\t\t\r\n\u27A1\uFE0F !admin = Menu Admin\uD83C\uDFC5\r\n\u27A1\uFE0F !menu1 = Em Breve \uD83C\uDF1E\r\n\u27A1\uFE0F !menu2 = Downloader Menu (OFF)\uD83C\uDF9E\r\n\u27A1\uFE0F !menu3 = Em Breve \uD83C\uDF87\r\n\u27A1\uFE0F !menu4 = Em Breve \uD83D\uDCD5",
+	menu1: "*Bem vindo ao Menu1*\r\n   \r\n*!sticker Basta enviar uma foto e na legenta escrever !sticker*",
+	//menu2: "*Welcome To Downloader Menu*\r\n   \r\n *!yt url* : Mendownload video dari youtube\r\ncontoh : !yt https:\/\/youtu.be\/K9jR4hSCbG4\r\n\r\n*!ytmp3 url* : Mendownload mp3 dari youtube\r\ncontoh : !ytmp3 https:\/\/youtu.be\/xUVz4nRmxn4\r\n\r\n*!fb url* : Mendownload video dari facebook\r\ncontoh : !fb url\r\n\r\n*!ig url* : Mendownload media foto\/video dari instagram\r\ncontoh : !ig url\r\n\r\n*!pin url* : Mendownload video dari pinterest\r\ncontoh : !pin url",
+	//menu3: "*!nama* : Melihat arti dari nama kamu\r\n contoh : !nama Bondan\r\n\r\n*!pasangan* : Check kecocokan jodoh\r\n contoh : !pasangan Dimas & Dinda"
+}
